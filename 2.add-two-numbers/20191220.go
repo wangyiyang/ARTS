af13662bac31ec028db6300152ae3f9cff08ed25
Wang Yiyang <wangyiyang.kk@aliyun.com>
2019-12-20 18:45:07 +0800

    add 2

---
+++ 2.add-two-numbers/20191220.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{0, nil}
+	current := head
+	carry := 0
+	for l1 != nil || l2 != nil || carry > 0 {
+		sum := carry
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+		carry = sum / 10
+		current.Next = new(ListNode)
+		current.Next.Val = sum % 10
+		current = current.Next
+	}
+	return head.Next
+
+}
+
+func main() {
+	l1 := ListNode{2, &ListNode{4, &ListNode{3, nil}}}
+	l2 := ListNode{5, &ListNode{6, &ListNode{4, nil}}}
+	result := addTwoNumbers(&l1, &l2)
+	for {
+		fmt.Println(result.Val)
+		if result.Next == nil {
+			break
+		}
+		result = result.Next
+	}
+
+}
