71c734c55f84437f43f3cc9e7dacc759288a5cd3
Wang Yiyang <wangyiyang.kk@aliyun.com>
2020-01-06 10:34:03 +0800

    760.find-anagram-mappings

---
+++ 760.find-anagram-mappings/20200106.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func anagramMappings(A []int, B []int) []int {
+	m := map[int]int{}
+	for i, n := range B {
+		m[n] = i
+	}
+	out := make([]int, len(A))
+	for i, n := range A {
+		out[i] = m[n]
+	}
+	return out
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
