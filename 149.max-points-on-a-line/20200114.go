486142a3c21d7e779e93a0d1fa0914e4f146dbbb
Wang Yiyang <wangyiyang.kk@aliyun.com>
2020-01-14 15:13:56 +0800

    149.max-points-on-a-line

--- 149.max-points-on-a-line/20200114.go
+++ 149.max-points-on-a-line/20200114.go
@@ -26,8 +26,8 @@ func gcd(x int, y int) int {
 func maxPoints(points [][]int) int {
 	var ans = 0
 	for i := 0; i < len(points); i++ {
-		var num int = 0
-		var maxer int = 0
+		var num int = 0   // 同一个点的数量
+		var maxer int = 0 // 和当前点在一条直线的最多点
 		recorder := make(map[Slope]int)
 		for j := 0; j < len(points); j++ {
 			if points[j][0] == points[i][0] && points[j][1] == points[i][1] {
