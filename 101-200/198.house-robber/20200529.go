2e770e3103a1f00e01f1356b7753c4dd44a19991
Wang Yiyang <wangyiyang.kk@aliyun.com>
2020-05-30 13:55:22 +0800

    198.house-robber

---
+++ 101-200/198.house-robber/20200529.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	dp[1] = max(nums[0], nums[1])
+	for i := 2; i < len(nums); i++ {
+		dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+	}
+	return dp[len(nums)-1]
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+	fmt.Println(rob([]int{1, 2, 3, 1}))
+}
