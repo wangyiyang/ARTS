c5a39e1855192f2406aa7cfc1d38571fc1f59d65
Wang Yiyang <wangyiyang.kk@aliyun.com>
2020-06-19 18:28:34 +0800

    125.valid-palindrome

---
+++ 101-200/125.valid-palindrome/20200618.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+	var sgood string
+	for i := 0; i < len(s); i++ {
+		if isalnum(s[i]) {
+			sgood += string(s[i])
+		}
+	}
+
+	n := len(sgood)
+	sgood = strings.ToLower(sgood)
+	for i := 0; i < n/2; i++ {
+		if sgood[i] != sgood[n-1-i] {
+			return false
+		}
+	}
+	return true
+}
+
+func isalnum(ch byte) bool {
+	return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
+func main() {
+	fmt.Println(isPalindrome("A man, a plan, a canal: Panama"))
+}
