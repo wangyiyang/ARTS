dcbf4c654b7977c4b68651f026ce9776b9cbf30b
Wang Yiyang <wangyiyang.kk@gmail.com>
2021-06-07 20:47:36 +0800

    add 494.target-sum

---
+++ 401-500/494.target-sum/20210607/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func findTargetSumWays(nums []int, target int) (count int) {
+	var backtrack func(int, int)
+	backtrack = func(index, sum int) {
+		if index == len(nums) {
+			if sum == target {
+				count++
+			}
+			return
+		}
+		backtrack(index+1, sum+nums[index])
+		backtrack(index+1, sum-nums[index])
+	}
+	backtrack(0, 0)
+	return
+}
+
+func main() {
+	fmt.Println(findTargetSumWays([]int{1, 1, 1, 1, 1}, 3))
+}
