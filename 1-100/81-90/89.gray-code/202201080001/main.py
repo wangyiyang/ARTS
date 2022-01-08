57061830ea73c5148ea6e7801f5dcca329b0d593
vagrant <vagrant@localhost.localdomain>
2022-01-08 11:38:48 +0000

    89.gray-code

---
+++ 1-100/81-90/89.gray-code/202201080001/main.py
@@ -0,0 +1,7 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        ans = [0]
+        for i in range(1, n + 1):
+            for j in range(len(ans) - 1, -1, -1):
+                ans.append(ans[j] | (1 << (i - 1)))
+        return ans
