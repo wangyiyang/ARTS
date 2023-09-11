61c0334e7b3a4b300be98978abfbc6ad62dbf850
Wang Yiyang <wangyiyang.kk@outlook.com>
2023-09-11 08:38:40 +0800

    add w5

---
+++ 2023/0911-0917/125-valid-palindrome/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        if (s == null || s.length() == 0) return true;
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            while (i < j && !Character.isLetterOrDigit(s.charAt(i))) ++i;
+            while (i < j && !Character.isLetterOrDigit(s.charAt(j))) --j;
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+            ++i;
+            --j;
+        }
+        return true;
+        
+    }
+
+    public static void main(String[] args) {
+        String s = "abcdcba";
+        Solution sol = new Solution();
+        System.out.println(sol.isPalindrome(s));
+    }
+
+}
+
+
