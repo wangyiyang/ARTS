4523774b2a4f7f706b6c049db2b5f7efbbcf3c99
Wang Yiyang <wangyiyang.kk@outlook.com>
2023-09-12 22:41:20 +0800

    update

---
+++ 2023/0911-0917/129-sum-root-to-leaf-number/Solution.java
@@ -0,0 +1,53 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    
+    TreeNode() {}
+    
+    TreeNode(int val) {
+        this.val = val;
+    }
+    
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+
+    }
+
+    private int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 创建树节点及其关系
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+
+        // 创建 Solution 对象
+        Solution solution = new Solution();
+
+        // 调用 sumNumbers 方法
+        int result = solution.sumNumbers(root);
+        System.out.println("Sum: " + result);
+    
+    }
+}
