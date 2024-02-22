c73387d8eaeb1cfbfa0451dad4f445b25c8e044a
Wang Yiyang <wangyiyang.kk@outlook.com>
2024-02-22 10:42:18 +0800

    update week02

---
+++ 2024/week02/Algorithm/56-merge-intervals/Solution/Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] currentInterval = intervals[0];
+        result.add(currentInterval);
+        for (int[] interval : intervals) {
+            int currentEnd = currentInterval[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+            if (currentEnd >= nextStart) {
+                currentInterval[1] = Math.max(currentEnd, nextEnd);
+            } else {
+                currentInterval = interval;
+                result.add(currentInterval);
+            }
+        }
+        return result.toArray(new int[result.size()][]);
+
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+        int[][] result = sol.merge(intervals);
+        for (int[] interval : result) {
+            System.out.println(Arrays.toString(interval));
+        }
+    }
+}
+
