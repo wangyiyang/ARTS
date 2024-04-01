f2f092d9f310bc56a5720362479245a9c10f79c6
Wang Yiyang <wangyiyang.kk@outlook.com>
2024-04-01 19:28:28 +0800

    add 146

---
+++ 2024/week05/Algorithm/146.lru-cache/LRUCache.java
@@ -0,0 +1,45 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class LRUCache {
+    int capacity;
+    Map<Integer, Integer> cache;
+
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+    
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+    
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1)); // returns 1
+        cache.put(3, 3); // evicts key 2
+        System.out.println(cache.get(2)); // returns -1 (not found)
+        cache.put(4, 4); // evicts key 1
+        System.out.println(cache.get(1)); // returns -1 (not found)
+        System.out.println(cache.get(3)); // returns 3
+        System.out.println(cache.get(4)); // returns 4
+                                           }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
