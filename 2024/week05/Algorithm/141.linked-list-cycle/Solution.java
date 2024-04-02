86eedf83f85168def8bd62798776c7c98da5b1a6
Wang Yiyang <wangyiyang.kk@outlook.com>
2024-04-02 22:51:23 +0800

    add 141

---
+++ 2024/week05/Algorithm/141.linked-list-cycle/Solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+        head.next.next.next.next = head.next;
+        System.out.println(solution.hasCycle(head));
+    }
+}
